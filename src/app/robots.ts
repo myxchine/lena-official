763004932a9929ed5b9144fef274c3c67fa83f8e
Michael Dos Santos <michael.dozzantoz@gmail.com>
2024-09-16 01:05:52 +0100

    t

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `https://lenaptzk.com/sitemap`,
+    sitemap: `https://lenaptzk.com/sitemap.xml`,
   };
 }
