428c8d87daf22bec77736533bab4a9a83ccf197a
Michael Dos Santos <michael.dozzantoz@gmail.com>
2024-09-16 01:28:13 +0100

    t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,26 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
-
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://lenaptzk.com",
+    title: "Lena Pietrzak",
+    description:
+      "I am Lena Barbara Pietrzak, a Fashion Model now in Paris, studying at IFM, Institut Français de la Mode. Originally from Poland and raised in Portugal, love London.",
+    images: [
+      {
+        url: "https://lenaptzk.com/opengraph-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Lena Pietrzak",
+      },
+    ],
+    siteName: "Lena Pietrzak",
+  },
   title: "Lena Pietrzak",
   description:
-    "Fashion Model in Paris studying at IFM, Institut Français de la Mode. Originally from Poland and raised in Portugal.",
+    "I am Lena Barbara Pietrzak, a Fashion Model now in Paris, studying at IFM, Institut Français de la Mode. Originally from Poland and raised in Portugal, love London.",
 };
 
 export default function RootLayout({
